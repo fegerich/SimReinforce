64416fc1210842ff27d54ee6826c4e82551af5d2
fegerich <Felix.Gerich@gmx.de>
2026-05-04 22:21:32 +0200

    add docstrings for tageszeit Klasse

--- Model/tageszeit.py
+++ Model/tageszeit.py
@@ -3,11 +3,22 @@ from dataclasses import dataclass
 
 @dataclass
 class Tageszeit:
-    start:        int
-    ende:         int
-    spawn_rate:   float
-    beschreibung: str
-    start_etagen: list
-    start_gewichtung:    list
-    ziel_etagen:  list
-    ziel_gewichtung:     list
+    """
+    Konfiguriert einen Zeitabschnitt des Simulationstages mit eigenen Spawn- und Verteilungsregeln.
+
+    start / ende:            Simulationszeitpunkte in Sekunden (0 = 08:00 Uhr)
+    spawn_rate:              Mittelwert der Exponentialverteilung für den Abstand zwischen zwei Spawns (in Sekunden)
+    beschreibung:            Anzeigename z.B. für die Visualisierung
+    start_etagen:            Liste der möglichen Startetagen
+    start_gewichtung:        Relative Gewichte für start_etagen (None = Gleichverteilung)
+    ziel_etagen:             Liste der möglichen Zieletagen
+    ziel_gewichtung:         Relative Gewichte für ziel_etagen (None = Gleichverteilung)
+    """
+    start:            int
+    ende:             int
+    spawn_rate:       float
+    beschreibung:     str
+    start_etagen:     list
+    start_gewichtung: list
+    ziel_etagen:      list
+    ziel_gewichtung:  list
